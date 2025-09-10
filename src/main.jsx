ec393e1c5bd2ed9829ddb675cffc41ff08f65d6b
NovaXTritan <divyanshukumar0163@gmail.com>
2025-09-10 22:02:16 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './styles/theme.css'
+import { useStore } from './state/store.js'
+import { applyTheme } from './utils/theme.js'
 
-const root = createRoot(document.getElementById('root'))
-root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-)
+function Root(){
+  // apply theme at startup and whenever it changes
+  const theme = useStore(s=>s.profile.theme)
+  React.useEffect(()=>applyTheme(theme), [theme])
+  return <HashRouter><App/></HashRouter>
+}
+
+createRoot(document.getElementById('root')).render(<Root/>)
